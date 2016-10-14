f3f0eaed98ccb6787ccbd58a09470d5e16546681
Tobias Kunze <rixx@cutebit.de>
2016-10-14 23:17:39 +0200

    Add cli command to create db

--- abbr/db.py
+++ abbr/db.py
@@ -27,3 +27,16 @@ def before_request():
 def close_db_connection(exception):
     if hasattr(g, 'db_connection'):
         g.db_connection.close()
+
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', 'r') as schema_file:
+            db.cursor().executescript(schema_file.read())
+        db.commit()
+
+
+@app.cli.command('initdb')
+def command_initdb():
+    init_db()
+    print('Initialized the database')
