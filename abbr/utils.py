3021a4a3e29291fe66fe8acbb73b13b8abdddf33
Tobias Kunze <rixx@cutebit.de>
2016-10-14 23:38:27 +0200

    Add helpers to get default values

--- abbr/utils.py
+++ abbr/utils.py
@@ -16,3 +16,24 @@ def return_json():
     )
     return best == 'application/json' and \
         request.accept_mimetypes[best] > request.accept_mimetypes['text/html']
+
+
+def get_random_name():
+    import db
+    conn = db.get_db_connection()
+    i = 10
+
+    while i > 0:
+        name = ''.join(
+            random.choice(app.config.get('ALPHABET'))
+            for _ in range(app.config.get('NAME_LENGTH'))
+            )
+        query = conn.execute('SELECT * FROM urls WHERE name = ?', (name, ))
+        if not query.fetchall():
+            return name
+    raise Exception('Unable to find unique name.')
+
+
+def get_expiry():
+    then = datetime.now() + app.config.get('EXPIRY')
+    return then.strftime('%Y-%m-%d %H:%M:%S')
