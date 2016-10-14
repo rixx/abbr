c7d9984bd8740dafc02c43743fb4c5250187a35c
Tobias Kunze <rixx@cutebit.de>
2016-10-14 23:39:03 +0200

    Add view for / (GET and POST)

---
+++ abbr/views.py
@@ -0,0 +1,53 @@
+from urllib.parse import urljoin
+
+from flask import abort, jsonify, redirect, render_template, request
+
+from app import app
+from db import get_url, write_url
+from utils import get_expiry, get_random_name, return_json
+from validation import (
+    ValidationError,
+    validate_expiry,
+    validate_name,
+    validate_url,
+)
+
+
+def home_page():
+    if return_json():
+        return jsonify("Hi, I'm abbr. "
+                       "Usage: POST either a url as string or a dict with "
+                       "'url' and optionally 'name' and 'expiry' "
+                       "('%Y-%m-%d %H:%M:%S').")
+    return render_template('index.html')
+
+
+def create_short_url():
+    if len(request.form) == 1 and 'url' not in request.form.keys():
+        url = next(request.form.keys())
+    else:
+        url = request.form.get('url')
+    name = request.form.get('name') or get_random_name()
+    expiry = request.form.get('expiry') or get_expiry()
+
+    try:
+        url = validate_url(url)
+        name = validate_name(name)
+        expiry = validate_expiry(expiry)
+    except ValidationError as err:
+        message = err.message
+    else:
+        write_url(name, url, expiry)
+        message = urljoin(app.config.get('DOMAIN'), name)
+
+    if return_json():
+        return jsonify(message)
+    return render_template('index.html', message=message)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if request.method == 'GET':
+        return home_page()
+    elif request.method == 'POST':
+        return create_short_url()
